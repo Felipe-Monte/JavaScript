2de99557e0720680161c7784283f09878fa07703
Carlos Felipe <109633306+Felipe-Monte@users.noreply.github.com>
2022-09-28 22:09:17 -0300

    Update arquivo.js

--- Udemy06 buscaArray/javascript/arquivo.js
+++ Udemy06 buscaArray/javascript/arquivo.js
@@ -6,21 +6,21 @@ var meusGatos = {
     gatos: [
         {
             "nome": 'Carlos',
-            'descricao': 'gato inteligente',
+            'descricao': 'Gato inteligente',
             'fotoUrl': 'images/gato-01.jpg',
             'linkExterno': 'https://www.instagram.com/felipe_mmonte/'
         },
 
         {
             'nome': 'Felipe',
-            'descricao': 'gato feliz',
+            'descricao': 'Gato feliz',
             'fotoUrl': 'images/gato-02.jpg',
             'linkExterno': 'https://www.instagram.com/felipe_mmonte/'
         },
 
         {
             'nome': 'Monte',
-            'descricao': 'gato alegre',
+            'descricao': 'Gato alegre',
             'fotoUrl': 'images/gato-03.jpg',
             'linkExterno': 'https://www.instagram.com/felipe_mmonte/'
         }
@@ -28,21 +28,21 @@ var meusGatos = {
 }
 
 function getGato() {
-    //Buscando valor da caixa de texto e coloando tudo em letras minúsculas.
     var texto = document.getElementById('texto').value.toLowerCase()
 
 
     //Loop para verificar cada gato do array e encontrar o que deseja.
     for (var i = 0; meusGatos.gatos.length > i; i++) {
 
-
         // if para: se o texto digitado for == ao nome de algum gato faça:
         if (texto == meusGatos.gatos[i].nome.toLowerCase()) {
             card.querySelector('h1').innerHTML = meusGatos.gatos[i].nome  
             card.querySelector('img').setAttribute('src', meusGatos.gatos[i].fotoUrl)  
-            return; // return para parar loop assim que for encontrado 
+            card.querySelector('p').innerHTML = meusGatos.gatos[i].descricao
+            return; 
         } else {
-            card.querySelector('h1').innerHTML = 'gato nao tem'
+            card.querySelector('h1').innerHTML = 'Gato não encontrado'
+            card.querySelector('p').innerHTML = 'Tente buscar outro gato'    
             
         }
     }
